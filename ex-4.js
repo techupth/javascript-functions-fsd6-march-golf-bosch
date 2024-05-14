d07ec04fd5e9ee020ce8686d96306807e234fa80
boschaiyadej <boschaiyadej@gmail.com>
2024-05-14 14:05:50 +0700

    feat: ex-js-function-test

--- ex-4.js
+++ ex-4.js
@@ -1,2 +1,18 @@
 // Start coding here
-let calculator = {};
+let calculator = {
+  add: (a, b) => {
+    return a + b;
+  },
+  subtract: (a, b) => {
+    return a - b;
+  },
+  multiply: (a, b) => {
+    return a * b;
+  },
+  divide: (a, b) => {
+    return a / b;
+  },
+};
+
+console.log(calculator.add(10, 20));
+console.log(calculator.divide(3000, 10));
